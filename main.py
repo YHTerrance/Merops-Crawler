6c7744a026488270e3289a74c4329ea094adef1e
B08902077 <b08902077@csie.ntu.edu.tw>
2019-12-26 19:53:28 +0800

    Aletered main.py so it can write to CSV file and also deal with '\xa0' empty characters.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from args import get_args
 
 
 if __name__ == '__main__':
-	args = get_args()
-	crawler = Crawler()
-	contents = crawler.crawl(args.start_date, args.end_date)
-	with open(args.output, "w") as fp:
-		for (date, title, content) in contents:
-			out_str = f'"{str(date)}", "{title}", "{content}"\n'
-			fp.write(out_str)
+    args = get_args()
+    crawler = Crawler()
+    contents = crawler.crawl(args.start_date, args.end_date)
+    with open(args.output, "w") as fp:
+        for (date, title, content) in contents:
+            content = content.replace('\xa0','')
+            out_str = f'"{str(date)}", "{title}", "{content}"\n'
+            fp.write(out_str)
 			
 
 	 # TODO: write content to file according to spec
