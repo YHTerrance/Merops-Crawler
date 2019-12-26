139e864c4e759ab9ac4d0ac93c0bf17e4ff667e3
YHTerrance <b08902073@csie.ntu.edu.tw>
2019-12-26 19:28:16 +0800

    Fix indentation problem in args.py. Implement simple function of main.

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from args import get_args
 
 
 if __name__ == '__main__':
-    args = get_args()
-    crawler = Crawler()
-    content = crawler.crawl(args.start_date, args.end_date)
-    # TODO: write content to file according to spec
+	args = get_args()
+	crawler = Crawler()
+	contents = crawler.crawl(args.start_date, args.end_date)
+	with open(args.output, "w") as fp:
+		for (date, title, content) in contents:
+			out_str = f'"{str(date)}", "{title}", "{content}"\n'
+			fp.write(out_str)
+			
+
+	 # TODO: write content to file according to spec
