3c4018a61d7462be1a88d071d0ad8c71a1d4eb82
YHTerrance <b08902073@csie.ntu.edu.tw>
2020-06-09 23:56:29 +0800

    Finish Scraper

---
+++ Makefile
@@ -0,0 +1,4 @@
+searcher: main.cpp
+	g++ -std=c++11 main.cpp -o searcher
+clean:
+	rm -rf searcher	
--- main.py
+++ main.py
@@ -1,21 +1,79 @@
-from crawler import Crawler
-from args import get_args
+import requests
+import pdb
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+
+from datetime import datetime 
+
+AA_table = {
+    "Ala": "A", "Arg": "R", "Asn": "N", "Asp": "D", "Cys": "C", "Gln": "Q",
+    "Glu": "E", "Gly": "G", "His": "H", "Ile": "I", "Leu": "L", "Lys": "K",
+    "Met": "M", "Phe": "F", "Pro": "P", "Pyl": "O", "Ser": "S", "Sec": "U",
+    "Thr": "T", "Trp": "W", "Tyr": "Y", "Val": "V", "Asx": "B", "Glx": "Z",
+    "Xaa": "X", "Xle": "J", "-" : "*"
+}
+
+def replace_AA(s, table):
+    for k, v in iter(table.items()):
+        s = s.replace(k, v)
+    return s
+
+replace_AA("Ala", AA_table)
 
 
-if __name__ == '__main__':
-    args = get_args()
-    crawler = Crawler()
-    contents = crawler.crawl(args.start_date, args.end_date)
-    with open(args.output, "w") as fp:
-        for (date, title, content) in contents:
-            title = title.replace('\n', '\r')
-            title = title.replace('\"','\"\"')
-            title = title.replace('\xa0','')
-            content = content.replace('\n', '\r')
-            content = content.replace('\"','\"\"')
-            content = content.replace('\xa0','')
-            out_str = f'"{str(date)}", "{title}", "{content}"\n'
-            fp.write(out_str)
-			
+path = os.getcwd()
+filename = os.path.join(path,'merops.csv')    
 
-	 # TODO: write content to file according to spec
+if os.path.exists(filename):
+    file = open(filename,"r+")
+    file.truncate(0)
+    file.close()
+
+baseUrl = "https://www.ebi.ac.uk/merops/cgi-bin/peptidase_specificity"
+
+r = requests.get(baseUrl)
+
+webContent = r.text;
+
+soup = BeautifulSoup(webContent, 'lxml')
+table = soup.find_all('table')[0]
+
+merops = []
+
+iterrows = iter(table.find_all('tr'))
+next(iterrows)
+for row in iterrows:
+    columns = row.find_all('td');    
+    merops.append([columns[0].get_text(), columns[2].get_text()])
+
+for merop in merops:
+    url = "https://www.ebi.ac.uk/merops/cgi-bin/substrates?id=" + merop[0]
+    r = requests.get(url)
+    webContent2 = r.text
+    soup = BeautifulSoup(webContent2, 'lxml')
+    table = soup.find_all('table')[0]
+    iterrows = iter(table.find_all('tr'))
+    next(iterrows)
+    cleavageSites = []
+    Ps = []
+    for row in iterrows:
+        columns = row.find_all('td')
+        cleavageSites.append(columns[3].get_text())
+        P = [columns[6].get_text(), columns[7].get_text(), columns[8].get_text(), columns[9].get_text(), columns[10].get_text(), columns[11].get_text(), columns[12].get_text(), columns[13].get_text()]
+        for i in range(len(P)):
+            P[i] = replace_AA(P[i], AA_table)
+        Ps.append(P)
+    
+    df = pd.DataFrame(
+    {"cleavageSites" : cleavageSites,
+    "P4~P4'" : Ps,
+    })
+    df.head()
+    f = open(filename, "a")
+    f.write(merop[0])
+    f.close()
+    df.to_csv(filename, mode = 'a')
+
+
+
