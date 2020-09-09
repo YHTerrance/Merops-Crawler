fd84f5303d6b6f47b14408204d8e7c2341c8ab66
YHTerrance <b08902073@csie.ntu.edu.tw>
2020-09-10 00:08:10 +0800

    Seperate cleavage site and peptidase

--- count.cpp
+++ count.cpp
@@ -15,41 +15,42 @@ typedef pair<string, string> pss;
 
 int main() {
 
-    ifstream finPeptidases;
-    finPeptidases.open("Peptidases.txt");
+    ifstream fin;
+    fin.open("Peptidases.txt");
 
-    ofstream foutPeptidaseCut;
-    foutPeptidaseCut.open("PeptidaseCut.txt");
+    ofstream fout;
+    fout.open("CleavageSiteCut.txt");
 
-    if(!finPeptidases) {
+    if(!fin) {
         cout << "Cannot open Peptidases.txt" << endl;
         return -1;
     }
 
+    unordered_map<string, pii> peptidaseCut;
+    unordered_map<string, pii> cleavageSiteCut;
 
-    unordered_map<string, pii> peptidaseToCut;
     string input;
     string meropsID, cleavageSite;
     int count = 0;
     bool isAce2;
-    while(finPeptidases >> input) {
+    while(fin >> input) {
         //cout << input << endl;
         if(input == "========================") {
             if(count != 0) {
                 if(isAce2)
-                    peptidaseToCut[meropsID + " " + cleavageSite].first = count;
+                    cleavageSiteCut[meropsID + " " + cleavageSite].first += count;
                 else
-                    peptidaseToCut[meropsID + " " + cleavageSite].second = count;
+                    cleavageSiteCut[meropsID + " " + cleavageSite].second += count;
             }
             count = 0;
         }
         else if(input == "MeropsID:") {
-            finPeptidases >> input;
+            fin >> input;
             meropsID = input;
             //cout << "meropsID is" << meropsID << endl;
         }
         else if(input == "CleavageSite:") {
-            finPeptidases >> input;
+            fin >> input;
             cleavageSite = input;
         }
         else if(input == "Ace2") {
@@ -63,9 +64,48 @@ int main() {
         }
     }
 
-    for(auto it = peptidaseToCut.begin(); it != peptidaseToCut.end(); it++) {
-        foutPeptidaseCut << it -> first << " " << (it -> second).first << " " << (it -> second).second << endl;
+    for(auto it = cleavageSiteCut.begin(); it != cleavageSiteCut.end(); it++) {
+        fout << it -> first << " " << (it -> second).first << " " << (it -> second).second << endl;
     }
+
+    fin.close();
+    fout.close();
+
+    fin.open("Peptidases.txt");
+    fout.open("PeptidaseCut.txt");
+    count = 0;
+    while(fin >> input) {
+        //cout << input << endl;
+        if(input == "========================") {
+            if(count != 0) {
+                if(isAce2)
+                    peptidaseCut[meropsID].first += count;
+                else
+                    peptidaseCut[meropsID].second += count;
+            }
+            count = 0;
+        }
+        else if(input == "MeropsID:") {
+            fin >> input;
+            meropsID = input;
+        }
+        else if(input == "Ace2") {
+            isAce2 = true;
+        }
+        else if(input == "Spike") {
+            isAce2 = false;
+        }
+        else if(input == "from") {
+            count++;
+        }
+    }
+
+    for(auto it = peptidaseCut.begin(); it != peptidaseCut.end(); it++) {
+        fout << it -> first << " " << (it -> second).first << " " << (it -> second).second << endl;
+    }
+
+    fin.close();
+    fout.close();
 
     return 0;
 }
